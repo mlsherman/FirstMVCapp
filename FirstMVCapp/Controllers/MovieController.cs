e267246c43eb39141dcfd73f627937402e4d6e06
mlsherman <advancedautosec@yahoo.com>
2023-06-09 11:58:18 -0400

    Initial commit

---
+++ FirstMVCapp/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCapp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FirstMVCapp
+ My First MVC App
